f6552798d092add2b9ae8b5de65753783976390b
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2021-09-19 22:52:42 -0400

    update the schema that will connect to the database

---
+++ apps/odyssey-lift-off-api/src/schema.ts
@@ -0,0 +1,32 @@
+import { gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    "Query  to get tracks array for the homepage grid"
+    getTracks: [Track!]!
+  }
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track{
+    id: ID!
+    " Track's title"
+    title: String!
+    "Tack's main Author"
+    author: Author!
+    "Track's URL to main illustration to display in track card or track page"
+    thumbnail: String
+    "Track's approximate length to complete, in minutes"
+    length: Int
+    "Number of modules the Track contains"
+    modulesCount: Int
+  }
+
+  type Author{
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author;s profile picture url"
+    photo: String
+  }
+`;
+
+module.exports = typeDefs;
