5700ec0404abb713ed9a5c9ca4e00d900b42dadd
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2021-09-20 23:02:44 -0400

    defining a query

--- apps/odyssey-lift-off/src/app/pages/tracks.js
+++ apps/odyssey-lift-off/src/app/pages/tracks.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql } from '@apollo/client';
+
+export const TRACKS = gql`
+  query TracksQuery {
+    getTracks {
+      id
+      title
+      author {
+        photo
+        id
+        name
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
