cec9876405369bcef3e09c06fb4b82d905eaa5db
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2021-10-12 01:17:23 -0400

    update the api url to be relative since the server will be hosted in the relative same area

--- apps/ng-lift-off/src/app/app.module.ts
+++ apps/ng-lift-off/src/app/app.module.ts
@@ -1,13 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    GraphQLModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
--- apps/odyssey-lift-off/src/environments/environment.prod.ts
+++ apps/odyssey-lift-off/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://fullstack-castronauts.herokuapp.com/api'
+  apiUrl: '/api'
 };
