f039a55f1a5158b4e3e9640377910ceebd0d100f
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2021-10-24 16:53:43 -0400

    setup the nest js app to serve the api

--- apps/nest-api/src/app/app.module.ts
+++ apps/nest-api/src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot(
+      {
+        rootPath: join(__dirname, '..', 'odyssey-lift-off'),
+      },
+      {
+        rootPath: join(__dirname, '..', 'ng-lift-off'),
+        serveRoot: '/ng',
+      },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
