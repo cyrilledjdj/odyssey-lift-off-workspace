96ce165c42804bab85f40b7575f4ba59b99c59ba
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2022-05-07 18:40:28 -0400

    chore: [nx migration] update-jest-config-extensions

--- apps/odyssey-lift-off/jest.config.ts
+++ apps/odyssey-lift-off/jest.config.ts
@@ -1,10 +1,11 @@
 module.exports = {
   displayName: 'odyssey-lift-off',
-  preset: '../../jest.preset.js',
+
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/odyssey-lift-off',
+  preset: '../../jest.preset.ts',
 };
